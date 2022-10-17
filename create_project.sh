e3bd2af749e3c251ca89a5fa69f6199e92e18fa5
Alex Sh <shalex.work@gmail.com>
2022-10-17 17:07:24 +0300

    Set googletest version

--- create_project.sh
+++ create_project.sh
@@ -122,12 +122,13 @@ set_gtest()
         'add_subdirectory(tests)' \
         >> CMakeLists.txt
 
-    printf '%s\n' 'project('${PROJECT_NAME}'_test)' \
+    printf '%s\n' 'project(${PROJECT_NAME}_test)' \
         '' \
         'include(FetchContent)' \
         'FetchContent_Declare(' \
         '   googletest' \
         '   GIT_REPOSITORY https://github.com/google/googletest.git' \
+        '   GIT_TAG release-1.12.1' \
         ')' \
         '' \
         '# For Windows: Prevent overriding the parent project compiler/linker settings' \
