5e482591c5e44e3577decbe56951354c1eb0f910
Alex Sh <shalex.work@gmail.com>
2024-08-24 01:27:51 +0300

    Update CMake to set version number from latest git tag if exists

--- templates/cpp/cmake/SetVersion.cmake
+++ templates/cpp/cmake/SetVersion.cmake
@@ -1,5 +1,27 @@
+# Get the latest git tag
+execute_process(
+    COMMAND git describe --tags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_TAG
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# If VERSION is not defined, set it to the latest git tag or "0.0.0" if no tag is found
 if(NOT DEFINED VERSION)
-    set(VERSION "0.0.0" CACHE STRING "Version number")
+    if(GIT_TAG)
+        string(REGEX REPLACE "v" "" GIT_TAG ${GIT_TAG})
+        if(GIT_TAG MATCHES "-")
+            set(VERSION_DIRTY "-dirty")
+        else()
+            set(VERSION_DIRTY "")
+        endif()
+        string(REGEX REPLACE "^v" "" GIT_TAG ${GIT_TAG})
+        string(REGEX REPLACE "-.*" "" GIT_TAG ${GIT_TAG})
+        set(VERSION ${GIT_TAG} CACHE STRING "Version number")
+    else()
+        set(VERSION "0.0.0" CACHE STRING "Version number")
+        set(VERSION_DIRTY "")
+    endif()
 endif()
 
 # Parse major, minor, and patch version from VERSION
@@ -7,9 +29,12 @@ string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_MAJOR ${VERSION})
 string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR ${VERSION})
 string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH ${VERSION})
 
+message(STATUS "Version: v${VERSION}${VERSION_DIRTY}")
+
 # Add the compile definitions
 add_compile_definitions(
     APP_VERSION_MAJOR=${VERSION_MAJOR}
     APP_VERSION_MINOR=${VERSION_MINOR}
     APP_VERSION_PATCH=${VERSION_PATCH}
+    APP_VERSION_DIRTY="${VERSION_DIRTY}"
 )
--- templates/cpp/source/main.cpp
+++ templates/cpp/source/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "@cpp@" << " v" << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << "." << APP_VERSION_PATCH << std::endl;
+    std::cout << "@cpp@" << " v" << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << "." << APP_VERSION_PATCH << APP_VERSION_DIRTY << std::endl;
 
     return 0;
 }
