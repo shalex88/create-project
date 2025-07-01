b4f56ef7ec64d5ded6e2b500897af9c195f7ef9e
Alex Sh <shalex.work@gmail.com>
2025-07-01 12:10:12 +0300

    Fix building release dependencies via vcpkg

--- templates/vcpkg/cmake/EnableVcpkg.cmake
+++ templates/vcpkg/cmake/EnableVcpkg.cmake
@@ -1,7 +1,4 @@
 # Should be included only from the top level cmake and before project()
-set(VCPKG_TARGET_TRIPLET x64-linux-release CACHE STRING "")
-set(VCPKG_HOST_TRIPLET x64-linux-release CACHE STRING "")
-
 configure_file(
         ${CMAKE_SOURCE_DIR}/vcpkg.json.in
         ${CMAKE_SOURCE_DIR}/vcpkg.json
